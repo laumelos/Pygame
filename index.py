ba52264fd79dc7dc1527d92951c0d34b8b65b475
laumelos <lauraemelos@gmail.com>
2022-07-06 11:22:41 -0300

    button

--- index.py
+++ index.py
@@ -12,11 +12,26 @@ largura = 640
 altura = 480
 x = 0
 y = 0
+telainicial=True
 
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Tamagochi<3")
 
-while True:
+class Botao(pygame.sprite.Sprite):
+    def __init__(self,*groups):
+        self.rect = pygame.draw.rect(tela,(235,103,155,90),(xbut,ybut,largbut,altbut)).convert_alpha()
+        self.touche = False
+
+        if self.rect.collidepoit (self.MousePos):
+            if self.self.mouse[0]:
+                self.touche = True
+                pygame.mouse.get_rel()
+            else: 
+                self.touche = False
+
+        pass
+
+while telainicial==True:
     for event in pygame.event.get():
         if event.type== QUIT:
             pygame.quit()
@@ -37,6 +52,29 @@ while True:
     tela.blit(coraçao,(320,30))
 
     tela.blit(coraçao,(440,70))
+
+    #button
+    xbut = 240
+    ybut = 360
+    largbut = 150
+    altbut = 60
+
+    '''
+
+    pygame.draw.rect(tela,(235,103,155,90),(xbut,ybut,largbut,altbut))
+
+    self.mouse = pygame.mouse.get_pressed()
+    self.MousePos = pygame.mouse.get_pos()
+
+
+    
+    if(mouse[0] > x and mouse[0] < x+largura and mouse[1] > y and mouse[1] < y+altura):
+        telainicial==False
+        pygame.draw.rect(tela,(0,0,0),(xbut,ybut,largbut,altbut))
+  '''
+     
+
+
     
     #texto
     txt='pet.com'                                 
