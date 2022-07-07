f3f373429c907f2bf54c32bda9fc199a6b838b76
laumelos <lauraemelos@gmail.com>
2022-07-07 09:59:45 -0300

    new images

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ pygame.init()
 white = (255,255,255)
 coraçao = pygame.image.load ("coraçao.png")
 gatinho = pygame.image.load ("gatinho.png")
+gatinhomaior = pygame.image.load ("gatinhomaior.png")
 fundo = pygame.image.load ("fundojogo.png")
 largura = 640
 altura = 480
@@ -61,35 +62,14 @@ while telainicial==True:
         if event.type == pygame.QUIT:
             telainicial=False
     pygame.time
-    #atualiz tela
-    pygame.draw.rect(tela, (white), (0,0,largura,altura))
+    
     '''
     imgbutton = pygame.image.load ("buttonjogo.png")
     tela.blit(imgbutton,(240, 360))
-    '''    
-    #corações
-    if y  >= altura:
-        y = 0
-    y = y + 1
- 
-    tela.blit(coraçao,(x,y))
- 
-    tela.blit(coraçao,(320,-80 + y))
- 
-    tela.blit(coraçao,(440,-40 + y))
- 
+    '''   
     #gatinho
-    tela.blit(gatinho,(245,200))
- 
-    #texto
-    txt='pet.com'                                
-    pygame.font.init()                                
-    fonte=pygame.font.get_default_font()              ##### fonte padrão
-    fontesys=pygame.font.SysFont(fonte, 60)           ##### fonte
-    txttela = fontesys.render(txt, 1, (0,0,0))        ##### cor
-    tela.blit(txttela,(240,100))                      ##### posição
-    pygame.display.update()                        
- 
+    tela.blit(gatinhomaior,(200,140))
+
     pygame.display.update()
  
 pygame.quit()
