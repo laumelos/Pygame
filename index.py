3bbe42d6d2ec6ecce4b1c5d55c78eb35ae421980
laumelos <lauraemelos@gmail.com>
2022-07-08 17:20:07 -0300

    organizar código

--- index.py
+++ index.py
@@ -9,9 +9,14 @@ pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
- 
 #variáveis
 white = (255,255,255)
+largura = 640
+altura = 480
+x = 0
+y = 0
+
+#imagens load
 bubbles = pygame.image.load ('images/bolhas.png')
 coraçao = pygame.image.load ('images/coraçao.png')
 gatinho = pygame.image.load ('images/gatinho.png')
@@ -19,14 +24,11 @@ gatinhomaior = pygame.image.load ('images/gatinhomaior.png')
 gatinhodormindo =  pygame.image.load ('images/gatinhodormindo.png')
 fundo = pygame.image.load ("images/fundojogo.png")
 fundoazul = pygame.image.load ('images/fundoazul.png')
+
+#sounds load
 bubblesound = pygame.mixer.Sound('sounds/539823__ristooooo1__bubbles-001.wav')
 sleepsound = pygame.mixer.Sound('sounds/gatinhodormindo.wav')
 
-largura = 640
-altura = 480
-x = 0
-y = 0
- 
 #tela
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("pet.com")
@@ -60,29 +62,34 @@ class button():
  
         return action
  
-#button water
-water_img = pygame.image.load("images/agua.png").convert_alpha()
-water_img = button(120, 395, water_img, 1)
+#button agua
+agua_img = pygame.image.load("images/agua.png").convert_alpha()
+agua_img = button(120, 395, agua_img, 1)
 
+#button morango
+morango_img = pygame.image.load ("images/morango.png").convert_alpha()
+morango_img = button(280, 390, morango_img, 1)
 
+#button lua
 lua_img = pygame.image.load("images/lua.png").convert_alpha()
 lua_img = button(460, 390,lua_img, 1)
 
- 
 #TELA
 telainicial=True
 while telainicial==True:
     
-    if (water_img.draw(tela)):    
+    #funcionalid banho
+    if (agua_img.draw(tela)):    
 
-        tela.blit(bubbles,(210, 80))
+        tela.blit(bubbles,(210, 80))                           
         pygame.mixer.Sound.play(bubblesound)
         pygame.mixer.music.stop()
-        pygame.display.update()
+        pygame.display.update()                                 
         time.sleep(6)
         tela.blit(gatinhomaior,(200,140))
         tela.blit(fundo,(0,0))
     
+    #funcionalid dormir
     if (lua_img.draw(tela)):    
 
         tela.blit(fundoazul,(0,0))
@@ -102,10 +109,9 @@ while telainicial==True:
    #button images
     water = pygame.image.load ("images/agua.png")
     tela.blit(water,(120, 395))
-    #time.sleep(5)
 
-    morango_img = pygame.image.load ("images/morango.png")
-    tela.blit(morango_img,(280, 390))
+    morango = pygame.image.load ("images/morango.png")
+    tela.blit(morango,(280, 390))
 
     lua = pygame.image.load ("images/lua.png")
     tela.blit(lua,(460, 390))
